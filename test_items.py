7136e25f486acea9fc4db6f5354652d8b6a900b7
werwolw <54941022+werwolw@users.noreply.github.com>
2020-10-18 15:50:33 +0500

    Update test_items.py

--- test_items.py
+++ test_items.py
@@ -5,5 +5,5 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 def test_guest_should_see_button_add_to_cart_pass(browser):
     browser.get(link)
     time.sleep(10)
-    browser.find_element_by_css_selector(".btn-add-to-basket")
+    button = browser.find_element_by_css_selector(".btn-add-to-basket")
     assert button, "Кнопка не найдена"
